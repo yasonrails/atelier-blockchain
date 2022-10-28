f014e871726ff1a5754acd42b0aae8f5f6f872ab
yasonrails <yasbernays@gmail.com>
2022-10-28 22:42:18 +0200

    add transaction

--- blockchain.rb
+++ blockchain.rb
@@ -4,18 +4,19 @@ require 'pp'
 LEDGER =[]
 
 class Block
-    attr_reader :index, :nonce, :timestamp, :data, :previous_data, :hash
+    attr_reader :index, :nonce, :timestamp, :transaction, :transaction_count, :previous_transaction, :hash
 
-    def initialize(index, data, previous_hash)
+    def initialize(index, transaction, previous_hash)
      @index = index
      @timestamp = Time.now
-     @data = data
+     @transaction = transaction
+     @transaction_count = transaction.size
      @previous_hash = previous_hash
      @hash, @nonce = compute_hash_with_proof_of_work
 
     end
     
-    def compute_hash_with_proof_of_work(difficulty="0011")
+    def compute_hash_with_proof_of_work(difficulty="00")
       nonce = 0 
       loop do
         hash = compute_hash_with_nonce(nonce)
@@ -34,8 +35,10 @@ class Block
       sha.update( @index.to_s + 
                   nonce.to_s +
                   @timestamp.to_s + 
-                  @data + 
-                  @previous_hash )
+                  @transaction.to_s +
+                  @transaction_count.to_s + 
+                  @previous_hash
+                )
       sha.hexdigest         
     end
     def self.first(data)
@@ -47,10 +50,10 @@ class Block
     end
         
 end # class Block
-
+#private 
 def create_first_block
   i = 0
-  instance_variable_set("@b#{i}", Block.first("BLK"))   
+  instance_variable_set("@b#{i}", Block.first({ from: "vendeur", to: "acheteur", what:"BTC", qty: "100_000" }))   
   LEDGER << @b0
   pp @b0
   add_block
